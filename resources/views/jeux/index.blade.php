269db3b3692abb9e89d95b2117dbf609139a7785
lucas_fardello <Fardello>
2020-12-16 14:11:50 +0100

    show dans l'index2

--- resources/views/jeux/index.blade.php
+++ resources/views/jeux/index.blade.php
@@ -10,11 +10,17 @@
 <h2>La liste des jeux</h2>
     <table border="1px">
         <tr>
-            <td>nom</td><td>description</td><td>url_media</td><td>theme</td><td>durée</td><td>nombre de joueurs</td>
+            <td>nom</td><td>description</td><td>url_media</td><td>theme</td><td>durée</td><td>nombre de joueurs</td><td></td>
         </tr>
         @foreach($jeux as $jeu)
             <tr>
-                <td><a href = {{route('jeux.show',$jeu->id)}}>{{$jeu->nom}}</a></td><td> {{$jeu->description}}</td> <td>{{$jeu->url_media}}</td> <td>{{$jeu->theme_id}}</td><td>{{$jeu->duree}}</td><td>{{$jeu->nombre_joueurs}}</td>
+                <td>{{$jeu->nom}}</td>
+                <td> {{$jeu->description}}</td>
+                <td>{{$jeu->url_media}}</td>
+                <td>{{$jeu->theme_id}}</td>
+                <td>{{$jeu->duree}}</td>
+                <td>{{$jeu->nombre_joueurs}}</td>
+                <td><a href = {{route('jeux.show',$jeu->id)}}>Plus d'infos</a></td>
             </tr>
         @endforeach
     </table>
