45ceaeec006e5dd7bf63496d220d055939ecce86
Benhamza Romain <romain_benhamza@ens.univ-artois.fr>
2020-12-16 13:24:12 +0100

    show debug

--- resources/views/jeux/show.blade.php
+++ resources/views/jeux/show.blade.php
@@ -1,11 +1,13 @@
 <div>
-    <p><strong>Titre  : </strong>{$jeu->nom}</p>
-    <p><strong>Description : </strong>{$jeu->nom}</p>
-    <p><strong>Illustration: : </strong>{$jeu->url_media}</p>
-    <p><strong>Regles : </strong>{$jeu->regles}</p>
-    <p><strong>langue : </strong>{$jeu->langue}</p>
-    <p><strong>Age conseillé : </strong>{$jeu->age}</p>
-    <p><strong>Nombre Joueurs : </strong>{$jeu->nombre_joueurs}</p>
-    <p><strong>Categorie : </strong>{$jeu->categorie}</p>
-    <p><strong>Duree : </strong>{$jeu->duree}</p>
+    <p><strong>Titre  : </strong>{{$jeu->nom}}</p>
+    <p><strong>Description : </strong>{{$jeu->nom}}</p>
+    <p><strong>Themes : </strong>{{$jeu->themes}}</p>
+    <p><strong>Mecaniquesd : </strong>{{$jeu->mecaniques}}</p>
+    <p><strong>Illustration: : </strong>{{$jeu->url_media}}</p>
+    <p><strong>Regles : </strong>{{$jeu->regles}}</p>
+    <p><strong>langue : </strong>{{$jeu->langue}}</p>
+    <p><strong>Age conseillé : </strong>{{$jeu->age}}</p>
+    <p><strong>Nombre Joueurs : </strong>{{$jeu->nombre_joueurs}}</p>
+    <p><strong>Categorie : </strong>{{$jeu->categorie}}</p>
+    <p><strong>Duree : </strong>{{$jeu->duree}}</p>
 </div>
