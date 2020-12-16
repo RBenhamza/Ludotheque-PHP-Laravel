e33d2faec090a2cc685c6078f861b794656e9165
Dubois Herve <herve_dubois1@ens.univ-artois.fr>
2020-12-16 19:35:35 +0100

    profil update

--- resources/views/jeux/profil.blade.php
+++ resources/views/jeux/profil.blade.php
@@ -3,6 +3,11 @@
 @section('navbar')
     @parent
 @endsection
+@php
+    $results = DB::select(DB::raw('select jeux.nom,achats.date_achat,achats.prix,achats.lieu from jeux inner join achats on jeux.id=achats.jeu_id where achats.user_id=1
+
+'));
+@endphp
 @section('content')
 <html>
     <head>
@@ -12,5 +17,18 @@
         <p><strong>Nom  : </strong>{{$user->name}}</p>
         <p><strong>email : </strong>{{$user->email}}</p>
     </div>
+
+    <div>
+        <p><strong>Nom  : </strong>{{$user->name}}</p>
+        <p><strong>email : </strong>{{$user->email}}</p>
+        <p>Mes jeux :</p>
+        @foreach($results as $jeu)
+            <p><strong>Nom :</strong>{{$jeu->nom}}<br> <strong>Date d'achat :</strong>{{$jeu->date_achat}} <strong>Prix :</strong>{{$jeu->prix}} <strong>Lieu de stockage :</strong>{{$jeu->lieu}}
+        @endforeach
+    </div>
 </html>
 @endsection
+
+
+
+
