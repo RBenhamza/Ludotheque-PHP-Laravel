30b454af54f3d6f5ed0c5295eed409178cf3aed4
lucas_fardello <Fardello>
2020-12-16 12:19:36 +0100

    create/store

--- app/Http/Controllers/JeuController.php
+++ app/Http/Controllers/JeuController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Editeur;
+use App\Models\Jeu;
+use App\Models\Theme;
+use Faker\Factory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class JeuController extends Controller
 {
@@ -19,22 +24,64 @@ class JeuController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function create()
     {
-        //
+        return view('jeux.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Routing\Redirector
      */
     public function store(Request $request)
     {
-        //
+
+        // validation des données de la requête
+        $this->validate(
+            $request,
+            [
+                'nom' => 'required',
+                'description' => 'required',
+                'regles' => 'required',
+                'langue' => 'required',
+                'age' => 'required',
+                'nombre_joueurs' => 'required',
+                'categorie' => 'required',
+                'duree' => 'required',
+            ]
+        );
+
+        // code exécuté uniquement si les données sont validaées
+        // sinon un message d'erreur est renvoyé vers l'utilisateur
+
+        // préparation de l'enregistrement à stocker dans la base de données
+        $theme_ids = Theme::all()->pluck('id')->get();
+        $editeur_ids = Editeur::all()->pluck('id')->get();
+        $faker = Factory::create('fr_FR');
+        $jeu = new Jeu;
+        $jeu->nom = $request->nom;
+        $jeu->description = $request->description;
+        $jeu->regles = $request->regles;
+        $jeu->langue = $request->langue;
+        $jeu->url_mediat = $request->url_media;
+        $jeu->age = $request->age;
+        $jeu->nombre_joueurs = $request->nombre_joueurs;
+        $jeu->categorie = $request->categorie;
+        $jeu->duree = $request->duree;
+        $jeu->user_id = Auth::id();
+        $jeu->theme_id = $faker->randomElement($theme_ids);
+        $jeu->editeur_id = $faker->randomElement($editeur_ids);
+
+        // insertion de l'enregistrement dans la base de données
+        $jeu->save();
+
+        // redirection vers la page qui affiche la liste des tâches
+        return redirect('/jeu.index');
+
     }
 
     /**
