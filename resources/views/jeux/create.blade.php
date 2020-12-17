4d1af8f76fa42eb374faa73037d9688dc7a106c8
antoine.longbien <antoine_longbien@ens.univ-artois.fr>
2020-12-17 16:18:06 +0100

    Creation jeux form

--- resources/views/jeux/create.blade.php
+++ resources/views/jeux/create.blade.php
@@ -12,46 +12,48 @@
     $authid=Auth::id();
     ?>
     <h1>Creation de jeu</h1>
-    <div class="item">
-        <p>Nom du jeu</p>
+    <div class="form-group">
+        <label>Nom du jeu</label>
         <div class="name-item">
-            <input type="text" name="nom" id="nom" value="{{ old('nom') }}"/>
+            <input  class="form-control" type="text" name="nom" id="nom" value="{{ old('nom') }}"/>
+            <small class="form-text text-muted">Entrez le nom d'un jeu.</small>
         </div>
 
     </div>
 
-    <div class="item">
-        <p>Description</p>
-        <textarea  name="description" id="description" rows="5">{{ old('description') }}</textarea>
+    <div class="form-group">
+        <label>Description</label>
+        <textarea class="form-control" name="description" id="description" rows="5">{{ old('description') }}</textarea>
+
     </div>
 
     <div class="item">
-        <p>Règles</p>
-        <input type="text" name="regles" id="regles" value="{{ old('regles') }}">
+        <label>Règles</label>
+        <input class="form-control" type="text" name="regles" id="regles" value="{{ old('regles') }}">
     </div>
 
     <div class="item">
-        <p>Langue</p>
-        <input type="text" name="langue" id="langue" value="{{ old('langue') }}">
+        <label>Langue</label>
+        <input class="form-control" type="text" name="langue" id="langue" value="{{ old('langue') }}">
     </div>
 
     <div class="item">
-        <p>Url_Media</p>
-        <input type="text" name="url_media" id="url_media" value="{{ old('url_media') }}">
+        <label>Url_Media</label>
+        <input class="form-control" type="text" name="url_media" id="url_media" value="{{ old('url_media') }}">
     </div>
 
     <div class="item">
-        <p>Age minimum</p>
-        <input type="text" name="age" id="age" value="{{ old('age') }}">
+        <label>Age minimum</label>
+        <input class="form-control" type="text" name="age" id="age" value="{{ old('age') }}">
     </div>
 
 
     <div class="item">
-        <p>Nombre de joueurs</p>
-        <input type="text" name="nombre_joueurs" id="nombre_joueurs" value="{{ old('nombre_joueurs') }}">
+        <label>Nombre de joueurs</label>
+        <input class="form-control" type="text" name="nombre_joueurs" id="nombre_joueurs" value="{{ old('nombre_joueurs') }}">
     </div><br>
     <div class="item">
-        <select id="theme_id" name="theme_id">
+        <select class="form-control" id="theme_id" name="theme_id">
             <option value="none">--Theme--</option>
             @foreach($themes as $theme)
                 <option value="{{$theme->id}}">{{$theme->nom}}</option>
@@ -59,21 +61,23 @@
         </select>
     </div><br>
     <div class="item">
-        <p>Catégorie</p>
-        <input type="text" name="categorie" id="categorie" value="{{ old('categorie') }}">
+        <label>Catégorie</label>
+        <input class="form-control" type="text" name="categorie" id="categorie" value="{{ old('categorie') }}">
     </div>
     <div class="item">
-        <p>Durée</p>
-        <input type="text" name="duree" id="duree" value="{{ old('duree') }}">
+        <label>Durée</label>
+        <input class="form-control" type="text" name="duree" id="duree" value="{{ old('duree') }}">
     </div>
     <div class="item">
-        <p>Editeur</p>
-        <input type="text" name="editeur" id="editeur" value="{{ old('editeur') }}">
+        <label>Editeur</label>
+        <input class="form-control" type="text" name="editeur" id="editeur" value="{{ old('editeur') }}">
     </div>
-
+<br>
     <div class="btn-block">
-        <a href="{{route("jeux.index")}}" class="btn-href">Retour</a>
-        <button type="submit" href="/">Valider</button>
+        <a href="{{route("jeux.index")}}" class="btn-href"><button type="button" class="btn btn-success">Retour</button>
+        </a>
+        <button class="btn btn-success" type="submit" href="/" >Valider</button>
+
     </div>
 </form>
 
