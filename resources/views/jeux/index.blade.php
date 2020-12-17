cad5f5ddc411f41a8be3b27aa6058e22dd69465f
Mateo Antonin <antonin_mateo@ens.univ-artois.fr>
2020-12-17 13:36:12 +0100

    Update index.blade.php

--- resources/views/jeux/index.blade.php
+++ resources/views/jeux/index.blade.php
@@ -20,39 +20,44 @@
 
 <br>
     <br>
-    <form action="{{route('jeux.index')}}" method="GET">
-        <label for="selectTri"><h4>Trier par : </h4></label>
-            <select class="form-control" id="selectTri" name="tri" onchange="this.form.submit()">
-                <option value="none">--Trier par--</option>
-                <option value="nom">nom</option>
-                <option value="nombre_joueurs">nombre de joueurs</option>
-                <option value="duree">durée</option>
-            </select>
-    </form>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 col-md-4 col-xl-4">
+                    <form action="{{route('jeux.index')}}" method="GET">
+                        <label for="selectTri"><h4>Trier par : </h4></label>
+                        <select class="form-control" id="selectTri" name="tri" onchange="this.form.submit()">
+                            <option value="none">--Trier par--</option>
+                            <option value="nom">nom</option>
+                            <option value="nombre_joueurs">nombre de joueurs</option>
+                            <option value="duree">durée</option>
+                        </select>
+                    </form>
+                </div>
+                <div class="col-sm-12 col-md-4 col-xl-4">
+                    <form action="{{route('jeux.index')}}" method="GET">
+                        <label for="triEditeur"><h4>Choix de l'éditeur</h4></label>
+                        <select class="form-control" id="triEditeur" name="editeur_id" onchange="this.form.submit()">
+                            <option value="none">--Editeurs--</option>
+                            @foreach($editeurs as $editeur)
+                                <option value="{{$editeur->id}}" @if($editeur->id==$editeur_id) selected @endif>{{$editeur->nom}}</option>
+                            @endforeach
+                        </select>
+                    </form>
+                </div>
+                <div class="col-sm-12 col-md-4 col-xl-4">
+                    <form action="{{route('jeux.index')}}" method="GET">
+                        <label for="triTheme"><h4>Choix du thème</h4></label>
+                        <select class="form-control" id="triTheme" name="themes_id" onchange="this.form.submit()">
+                            <option value="none">--Themes--</option>
+                            @foreach($themes as $theme)
+                                <option value="{{$theme->id}}" @if($theme->id==$themes_id) selected @endif>{{$theme->nom}}</option>
+                            @endforeach
+                        </select>
+                    </form>
+                </div>
+            </div>
+        </div>
 </div>
-<div>
-    <form action="{{route('jeux.index')}}" method="GET">
-        <label for="triEditeur"><h4>Choix de l'éditeur</h4></label>
-            <select class="form-control" id="triEditeur" name="editeur_id" onchange="this.form.submit()">
-                <option value="none">--Editeurs--</option>
-                @foreach($editeurs as $editeur)
-                    <option value="{{$editeur->id}}" @if($editeur->id==$editeur_id) selected @endif>{{$editeur->nom}}</option>
-                @endforeach
-        </select>
-    </form>
-</div>
-<div>
-    <form action="{{route('jeux.index')}}" method="GET">
-        <label for="triTheme"><h4>Choix du thème</h4></label>
-            <select class="form-control" id="triTheme" name="themes_id" onchange="this.form.submit()">
-                <option value="none">--Themes--</option>
-                @foreach($themes as $theme)
-                    <option value="{{$theme->id}}" @if($theme->id==$themes_id) selected @endif>{{$theme->nom}}</option>
-                @endforeach
-        </select>
-    </form>
-</div>
-
 
 <body>
 <h2 class="display-4 text-center text-gray p-md-3">La liste des jeux</h2>
