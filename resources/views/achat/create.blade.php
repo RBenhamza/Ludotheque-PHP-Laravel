dd871012d63fa8e29d5c03aa1d8c0c712150dd4d
antoine.longbien <antoine_longbien@ens.univ-artois.fr>
2020-12-17 13:22:58 +0100

    Formulaire liste, achat

--- resources/views/achat/create.blade.php
+++ resources/views/achat/create.blade.php
@@ -9,45 +9,40 @@
     $jeux = App\Models\Jeu::all();
     $authid=Auth::id();
     ?>
-    <title>Creation d'un commentaire</title>
+    <title>Achat d'un jeu</title>
     <div class="container">
         <div class="row">
             <form action="{{route('achat.store')}}" method="POST">
                 {!! csrf_field() !!}
                 {{--<h1>Creation d'un commentaire pour le jeu {{$jeu->nom}}</h1>--}}
-                <div class="col-md-6">
-                    <div class="item">
-                        <p>Lieu</p>
-                        <div class="name-item">
-                            <input type="text" name="lieu" id="lieu" value="{{ old('lieu') }}"/>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="item">
-                        <p>Prix</p>
-                        <div class="name-item">
-                            <input type="text" name="prix" id="prix" value="{{ old('prix') }}"/>
-                        </div>
-                    </div>
-                </div>
+    </div>
+
+    <form>
+        <div class="form-group">
+            <label>Lieu</label>
+            <input class="form-control" type="text" name="lieu" id="lieu" value="{{ old('lieu') }}"/>
+            <small class="form-text text-muted">Veuillez entrer un lieu.</small>
         </div>
-        <div>
-            <select id="jeu" name="jeu">
-                <option value="none">--Jeu--</option>
-                @foreach($jeux as $jeu)
-                    <option value="{{$jeu->id}}">{{$jeu->nom}}</option>
-                @endforeach
-            </select>
+        <div class="form-group">
+            <label>Prix</label>
+            <input class="form-control" type="text" name="prix" id="prix" value="{{ old('prix') }}"/>
+            <small class="form-text text-muted">Veuillez entrer un prix.</small>
         </div>
+
+        <select class="form-control" id="jeu" name="jeu">
+            <option value="none">Sélectionnez un jeu : </option>
+            @foreach($jeux as $jeu)
+                <option value="{{$jeu->id}}">{{$jeu->nom}}</option>
+            @endforeach
+        </select>
+        <br>
         <div>
             <div class="btn-block">
                 <a href="{{route("profil",['id'=>$authid])}}" class="btn-href">Retour</a>
-                <button type="submit" href="/">Valider</button>
+                <button type="submit" href="/" class="btn btn-success">Valider</button>
             </div>
         </div>
-        </form>
-    </div>
+    </form>
 
 
 @endsection
--- resources/views/jeux/index.blade.php
+++ resources/views/jeux/index.blade.php
@@ -22,33 +22,33 @@
     <br>
     <form action="{{route('jeux.index')}}" method="GET">
         <label for="selectTri"><h4>Trier par : </h4></label>
-        <select id="selectTri" name="tri" onchange="this.form.submit()">
-            <option value="none">--Trier par--</option>
-            <option value="nom">nom</option>
-            <option value="nombre_joueurs">nombre de joueurs</option>
-            <option value="duree">durée</option>
-        </select>
+            <select class="form-control" id="selectTri" name="tri" onchange="this.form.submit()">
+                <option value="none">--Trier par--</option>
+                <option value="nom">nom</option>
+                <option value="nombre_joueurs">nombre de joueurs</option>
+                <option value="duree">durée</option>
+            </select>
     </form>
 </div>
 <div>
     <form action="{{route('jeux.index')}}" method="GET">
         <label for="triEditeur"><h4>Choix de l'éditeur</h4></label>
-        <select id="triEditeur" name="editeur_id" onchange="this.form.submit()">
-            <option value="none">--Editeurs--</option>
-            @foreach($editeurs as $editeur)
-                <option value="{{$editeur->id}}" @if($editeur->id==$editeur_id) selected @endif>{{$editeur->nom}}</option>
-            @endforeach
+            <select class="form-control" id="triEditeur" name="editeur_id" onchange="this.form.submit()">
+                <option value="none">--Editeurs--</option>
+                @foreach($editeurs as $editeur)
+                    <option value="{{$editeur->id}}" @if($editeur->id==$editeur_id) selected @endif>{{$editeur->nom}}</option>
+                @endforeach
         </select>
     </form>
 </div>
 <div>
     <form action="{{route('jeux.index')}}" method="GET">
         <label for="triTheme"><h4>Choix du thème</h4></label>
-        <select id="triTheme" name="themes_id" onchange="this.form.submit()">
-            <option value="none">--Themes--</option>
-            @foreach($themes as $theme)
-                <option value="{{$theme->id}}" @if($theme->id==$themes_id) selected @endif>{{$theme->nom}}</option>
-            @endforeach
+            <select class="form-control" id="triTheme" name="themes_id" onchange="this.form.submit()">
+                <option value="none">--Themes--</option>
+                @foreach($themes as $theme)
+                    <option value="{{$theme->id}}" @if($theme->id==$themes_id) selected @endif>{{$theme->nom}}</option>
+                @endforeach
         </select>
     </form>
 </div>
